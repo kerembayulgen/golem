3b9ef53a815601f00e85637cd1ac842ebb99f8fb
Kerem Bayülgen <kerembayulgen@outlook.com>
2025-06-05 15:42:06 +0200

    fix: incorrect brackets and color

--- process.py
+++ process.py
@@ -2,15 +2,12 @@ import os
 import re
 
 
-LATEX_REMOVE_PAR: str = r"\\(\[|\])"
 LATEX_REPLACE_MARKS: str = r":marks\[(\d+)\]"
-LATEX_REPLACE_BOLD: str = r"\*\*([^*\n]*?)\*\*"
-LATEX_REPLACE_BOLD_EDGE: str = r"\*\*((?:[^*]|\*(?!\*))*?)\*\*"
-LATEX_REPLACE_ITALIC: str = r"\_\_([^_\n]*?)\_\_"
-LATEX_REPLACE_ITALIC_EDGE: str = r"\_\_((?:[^_]|_(?!_))*?)\_\_"
-LATEX_REMOVE_NESTING: str = r"\$\$\s*\\begin\{([^}]+)\}(.*?)\\end\{\1\}\s*\$\$"
 LATEX_REPLACE_IMAGE: str = r"!\[([^\]]*)\]\(([^)]+)\)"
-LATEX_NESTING_ALT: str = r"$$\\begin{\1}\2\\end{\1}$$"
+LATEX_REMOVE_NESTING: str = (
+    r"\$\$\s*\\begin\{([a-zA-Z]+[*])\}\s*([\s\S]+?)\s*\\end\{\1\}\s*\$\$"
+)
+LATEX_REPLACE_ALIGN: str = r"(\\begin\{aligned\}[\s\S]*?\\end\{aligned\})"
 
 LATEX_FILE: str = r"""
 % !TeX TXS-program:compile = txs:///pdflatex/[-shell-escape]
@@ -28,6 +25,18 @@ LATEX_FILE: str = r"""
 \usepackage{bm}
 \usepackage{svg}
 \usepackage{tabto}
+\usepackage{tikz}
+\usepackage{siunitx}
+\usepackage[most]{tcolorbox}
+
+\newcommand{\answer}{}
+
+\newcommand{\cancel}[1]{%
+	\tikz[baseline=(X.base)]{
+		\node[inner sep=0pt, outer sep=0pt] (X) {$#1$};
+		\draw[red, thick] (X.south west) -- (X.north east);
+	}%
+}
 
 \TabPositions{1cm, 2cm, 3cm, 4cm}
 \makeatletter
@@ -64,12 +73,6 @@ def convert_indentation_to_tabs(
 
 def process_latex(inp: str) -> str:
     inp = LATEX_FILE + inp
-    inp = re.sub(LATEX_REMOVE_PAR, r"\1", inp)
-    inp = re.sub(LATEX_REPLACE_MARKS, r"\\hfill[\1]", inp)
-    inp = re.sub(LATEX_REPLACE_BOLD, r"\\textbf{\1}", inp)
-    inp = re.sub(LATEX_REPLACE_ITALIC, r"\\textit{\1}", inp)
-    inp = re.sub(LATEX_REPLACE_BOLD_EDGE, r"\\textbf{\1}", inp)
-    inp = re.sub(LATEX_REPLACE_ITALIC_EDGE, r"\\textit{\1}", inp)
 
     def replacement(match: re.Match[str]) -> str:
         alt_text = match.group(1)
@@ -84,9 +87,16 @@ def process_latex(inp: str) -> str:
     inp = re.sub(LATEX_REPLACE_IMAGE, replacement, inp)
     inp = inp.replace(":::center", "")
     inp = inp.replace(":::", "")
-    inp = re.sub(LATEX_REMOVE_NESTING, LATEX_NESTING_ALT, inp, flags=re.DOTALL)
-    inp = convert_indentation_to_tabs(inp)
-    inp = inp.replace("$$\\begin{align*}", "\\[\\begin{aligned}")
-    inp = inp.replace("\\end{align*}$$", "\\end{aligned}\\]")
+    inp = re.sub(LATEX_REPLACE_MARKS, r"\\hfill[\1]", inp)
+    inp = re.sub(r".\[Maximum mark: ([^a-z]*)\\]", r"[Maximum mark: \1]", inp)
+    inp = re.sub(r"\*\*([^*\n]*?)\*\*", r"\\textbf{\1}", inp)
+    inp = re.sub(LATEX_REMOVE_NESTING, r"\\begin{\1}\2\\end{\1}", inp, flags=re.DOTALL)
+
+    def tagstar_inside_aligned(m: re.Match[str]):
+        content = re.sub(r"\\tag\*\{([^}]*)\}", r"\1", m.group(0))
+        return content
+
+    inp = re.sub(LATEX_REPLACE_ALIGN, tagstar_inside_aligned, inp, flags=re.DOTALL)
+
     inp += LATEX_FILE_END
     return inp
